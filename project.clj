cfa098e1e07e816f8041e380f1a4fe414826cdce
ertucetin <ertu.ctn@gmail.com>
2020-03-28 02:40:50 +0300

    project.clj update

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
-(defproject patika "0.1.0-SNAPSHOT"
+(defproject patika "0.1.0"
 
   :description "FIXME: write description"
 
-  :url "http://example.com/FIXME"
+  :url "https://github.com/ertugrulcetin/patika"
 
   :author "Ertuğrul Çetin"
 
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
