91bae483dc4ce7e9390fd7f5fedcf827699d6fcc
ertucetin <ertu.ctn@gmail.com>
2020-03-28 02:45:26 +0300

    project.clj update

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org"}]
-                        ["releases" {:sign-releases false :url "https://clojars.org"}]
-                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
+  :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["releases" {:sign-releases false :url "https://clojars.org/repo"}]
+                        ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
 
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
